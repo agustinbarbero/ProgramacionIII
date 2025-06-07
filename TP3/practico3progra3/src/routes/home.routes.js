dbfdf6f8c7253e12f23ed1ec27055c62ee7d8a61
agustinbarbero <agustinbarbero932@gmail.com>
2025-06-07 17:09:42 -0300

    Actualizando TP3

--- TP3/practico3progra3/src/routes/home.routes.js
+++ TP3/practico3progra3/src/routes/home.routes.js
@@ -1,11 +1,12 @@
 const {Router} = require('express');
 const {turnos, pacientes} = require('../controllers/home/home.controller.js')
 const rutaHome = Router();
+
+rutaHome.get('/', (req, res) => {
+  res.redirect('/turnos');
+});
 rutaHome.get('/turnos', turnos);
 rutaHome.get('/pacientes', pacientes);
-
-//Otras rutas CRUD
-
 
 module.exports = rutaHome;
 
